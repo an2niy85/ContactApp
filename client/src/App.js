bc1402661538efef54d370cd80cc002a81f63c88
an2niy85 <an2niy85@gmail.com>
2026-02-10 21:06:48 +0400

    Добавление элементов в набор данных и использование react хуков

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import './App.css';
+import React, { useState } from 'react';
 import TableContact from './layout/TableContact/TableContact';
 
-const contacts = [
+const App = () => {
+
+  const [contacts, setContacts] = useState(
+    [
     {id:1, name:'Имя Фамилия 1', email: 'q1@e.rt'},
     {id:2, name:'Имя Фамилия 2', email: 'q2@e.rt'},
     {id:3, name:'Имя Фамилия 3', email: 'q3@e.rt'},
-];
+    ]
+  );
 
-const App = () => {
+  const addContact = () => {
+    const item = {
+      id:Math.floor(Math.random() * 100), 
+      name:'Имя Фамилия 3', 
+      email: 'q3@e.rt'
+    };
+    setContacts(...contacts, item);    
+  }
+
   return (
     <div className='container mt-5'>
       <div className='card'>
@@ -16,6 +28,14 @@ const App = () => {
         </div>
         <div className='card-body'>
           <TableContact contacts={contacts}/>
+          <div>
+            <button 
+              className='btn btn-primery'
+              onClick={()=>{addContact()}}
+            >
+              Добавить контакт
+            </button>
+          </div>
         </div>
       </div>
     </div>
