437ff14da417273147c5280e523ef3c9b5c89a7f
an2niy85 <an2niy85@gmail.com>
2026-02-17 09:10:32 +0400

    Настроили GET-запрос

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TableContact from './layout/TableContact/TableContact';
 import FormContact from './layout/FormContact/FormContact';
 
 const baseApiUrl = process.env.REACT_APP_API_URL;
 
 const App = () => {
-  const url =`${baseApiUrl}/contacts`;
-  console.log(url);
-  axios.get(url).then(
-    res => console.log(res.data)
-  );
+  
 
-  const [contacts, setContacts] = useState(
-    [
-    {id:1, name:'Имя Фамилия 1', email: 'q1@e.rt'},
-    {id:2, name:'Имя Фамилия 2', email: 'q2@e.rt'},
-    {id:3, name:'Имя Фамилия 3', email: 'q3@e.rt'},
-    ]
+  const [contacts, setContacts] = useState([]);
+
+  const url =`${baseApiUrl}/contacts`;  
+  useEffect(() => {
+    axios.get(url).then(
+    res => setContacts(res.data)
   );
+  }, []);
 
   const addContact = (contactName, contactEmail) => {
     
